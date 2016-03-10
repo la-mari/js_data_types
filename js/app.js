3a46cac914bf40f827645eaa4660e176edfad8d5
Mari Gonzalez <karima19@gmail.com>
2016-03-09 17:22:44 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+//var flowers = ["rose", "tulip", "peony", "orchid", "daffodil"];
+
+//var index = 0;
+//while(index < flowers.length) {
+  //console.log(flowers[index]);
+ // index ++;
+//}
+
+
+var beatles = ["George", "Ringo", "John", "Paul"];
+
+//var index = 0;
+//while(index < beatles.length) {
+  //console.log("Hi I'm" + " " + beatles[index]);
+ // index ++;
+//}
+
+
+for(var i = beatles.length - 1; i >= 0; i--) {
+  console.log("Hello, my name is " + beatles[i]);
+}
